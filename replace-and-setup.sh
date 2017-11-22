4ee8fa319bda4b854c9626ae323377386cbaaeb6
konaraddio <omkar.konaraddi@gmail.com>
2017-11-22 13:05:26 -0500

    added a script for easy deploying

---
+++ replace-and-setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# This script should only be sitting in the folder where the site will sit!
+git clone https://github.com/robotics-at-maryland/ram_website.git
+mv ram_website/_site/* .
