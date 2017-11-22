d73e917daeb17bb611c9b0dc846f9083de3ac9ab
konaraddio <omkar.konaraddi@gmail.com>
2017-11-22 13:07:53 -0500

    removed easy deployment script because it didn't work

--- replace-and-setup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# This script should only be sitting in the folder where the site will sit!
-git clone https://github.com/robotics-at-maryland/ram_website.git
-mv ram_website/_site/* .
